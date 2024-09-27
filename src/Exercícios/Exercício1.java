1812b126f8bb5aa02ab8070b4ba8bb3e4bc9d4ca
MateusVieira <mateusvs2420@gmail.com>
2024-09-27 09:31:44 -0300

    Aula 27/09/2024 TiposDeDados

---
+++ src/Exercícios/Exercício1.java
@@ -0,0 +1,16 @@
+package Exercícios;
+
+import static java.lang.System.*;
+
+public class Exercício1 {
+    public static void main(String[] args) {
+        int anoNasc = 2007;
+        double peso = 51.5;
+        char sexo = 'M';
+        double altura = 1.75;
+        System.out.println("anoNasc:" + anoNasc);
+        System.out.println("Peso: " + peso);
+
+
+    }
+}
