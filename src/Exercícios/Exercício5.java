10b3ad20e3989554501a03bb8493b423f96b0f7e
Mateus Vieira <mateusvs2420@gmail.com>
2024-10-17 15:00:52 -0300

    Exemplos de Estruturas de Decisão

--- src/Exercícios/Exercício5.java
+++ src/Exercícios/Exercício5.java
@@ -2,19 +2,7 @@ package Exercícios;
 
 public class Exercício5 {
     public static void main(String[] args) {
-
-        // Declare as três variáveis
-        int nota1 = 10;
-        int nota2 = 20;
-        int nota3 = 30;
-
-        // Calcule a soma total
-        int somaTotal = nota1 +  nota2 + nota3;
-
-        // Exibe o resultado
-        System.out.println("O soma total das provas é: " + somaTotal);
-
-    }
+}
 
 
 }
