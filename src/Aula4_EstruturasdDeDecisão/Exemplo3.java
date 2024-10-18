ed4fcfd1d032b826d311c06bbe6994308205dd16
Mateus Vieira <mateusvs2420@gmail.com>
2024-10-18 09:30:58 -0300

    18/10/2024

--- src/Aula4_EstruturasdDeDecisão/Exemplo3.java
+++ src/Aula4_EstruturasdDeDecisão/Exemplo3.java
@@ -5,28 +5,31 @@ import java.util.Scanner;
 public class Exemplo3 {
     public static void main(String[] args) {
 
-             Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-             // Recebe a idade da pessoa
-                   System.out.println("Digite sua idade: ");
-               int idade = scanner.nextInt();
+        // Recebe a idade da pessoa
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
 
-             // Classifica a Idade
-               if (idade >= 0 && idade <= 12) {
-                   System.out.println("Você é uma Criança");
+        // Classifica a Idade
+        if (idade >= 0 && idade <= 12) {
+            System.out.println("Você é uma Criança");
 
-             } else if (idade >= 13 && idade <=17) {
-                   System.out.println("Você é um(a) Adolescente");
+        } else if (idade >= 13 && idade <= 17) {
+            System.out.println("Você é um(a) Adolescente");
 
-             } else if (idade >= 18 && idade <=64) {
-                   System.out.println("Você é um Adulto(a)");
+        } else if (idade >= 18 && idade <= 64) {
+            System.out.println("Você é um Adulto(a)");
 
-             } else if (idade >= 65) {
-                   System.out.println("Você é um Idoso(a)");
+        } else if (idade >= 65) {
+            System.out.println("Você é um Idoso(a)");
 
-             scanner.close();
+        } else {
+            System.out.println("Sua idade é inválida");
 
-             }
-             }
-             }
+            scanner.close();
+
+        }
+    }
+}
 
