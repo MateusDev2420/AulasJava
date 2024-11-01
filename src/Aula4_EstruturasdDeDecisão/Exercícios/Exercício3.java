e455115f8b48aa93ea0ab58267dc7cc156a14d12
Mateus Vieira Souza <mateusvs2420@gmail.com>
2024-11-01 11:18:26 -0300

    .

--- src/Aula4_EstruturasdDeDecisão/Exercícios/Exercício3.java
+++ src/Aula4_EstruturasdDeDecisão/Exercícios/Exercício3.java
@@ -1,17 +1,37 @@
 package Aula4_EstruturasdDeDecisão.Exercícios;
 
+import java.util.Scanner;
+
 public class Exercício3 {
     public static void main(String[] args) {
-        int produto;
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\t1 Seja Bem Vindo ao nosso Menu de Eletrônicos");
-        System.out.println("\t2 Seja Bem Vindo ao nosso Menu de Alimentos");
-        System.out.println("\t3 Seja Bem Vindo ao nosso Menu de Vestuário");
+        System.out.println("\t1- Eletrônicos");
+        System.out.println("\t2- Alimentos");
+        System.out.println("\t3- Vestuário");
+
+
+        System.out.println("Digite o número do departamento que você deseja acessar: ");
+        int produto = scanner.nextInt();
 
         switch (produto) {
+
             case 1:
+                System.out.println("Seja Bem Vindo a nossa área de Eletrônicos!");
+                break;
+
+            case 2:
+                System.out.println("Seja Bem Vindo a nossa área de Alimentos!");
+                break;
+
+            case 3:
+                System.out.println("Seja Bem Vindo a nossa área de Vestuários!");
+                break;
+
+            default:
+                System.out.println("Código Inválido");
 
         }
-
+        scanner.close();
     }
 }
---
+++ src/Aula4_EstruturasdDeDecisão/Exercícios/Exercício4.java
@@ -0,0 +1,34 @@
+package Aula4_EstruturasdDeDecisão.Exercícios;
+
+import java.util.Scanner;
+
+public class Exercício4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int vendas;
+        Double comissão = 0.0;
+
+        System.out.println("Digite o valor total de suas vendas esse Mês: ");
+        vendas = scanner.nextInt();
+
+        if (vendas < 1000) {
+            comissão = vendas * 0.05;
+            System.out.println("Sua comissão será de: " + comissão);
+        }
+
+        else if ( vendas >= 1000 && vendas <= 5000) {
+            comissão = vendas * 0.10;
+            System.out.println("Sua comissão será de: " + comissão);
+
+        }
+
+        else if ( vendas >= 5000) {
+            comissão = vendas * 0.15;
+            System.out.println("Sua comissão será de: " + comissão);
+
+
+
+
+        }
+    }
+}
