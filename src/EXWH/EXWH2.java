803cde3e2a7f1070675eb7089e955e6cddac92bf
Mateus Vieira Souza <mateusvs2420@gmail.com>
2024-12-16 09:29:02 -0300

    16/12/2024

--- src/EXWH/EXWH2.java
+++ src/EXWH/EXWH2.java
@@ -1,7 +1,19 @@
 package EXWH;
 
+import java.util.Scanner;
+
 public class EXWH2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 1;
+        int soma = 0;
+        int N;
 
+        System.out.println("Digite um número N: ");
+          N = scanner.nextInt();
+        while (i <=N) {
+            soma = soma + i;
+            i++;
+        }
     }
 }
