4600224bc4c817dc5700bcbdf50674c4fd455db5
Mateus Vieira <mateusvs2420@gmail.com>
2025-01-02 02:27:23 -0300

    02/01/2024

--- src/Aula5_EstruturasDeRepetição/Exercício3.java
+++ src/Aula5_EstruturasDeRepetição/Exercício3.java
@@ -4,6 +4,31 @@ import java.util.Scanner;
 
 public class Exercício3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int NI;
+        int fatorial = 1;
+        int i = 1;
 
+        System.out.println("Digite um número inteiro positivo: ");
+        NI = scanner.nextInt();
+
+        if (NI <= 0) {
+            System.out.println("Por favor, insira um número inteiro positivo.");
+
+        } else {
+            System.out.print("Cálculo do fatorial (decrescente): ");
+
+            do {
+                fatorial = fatorial * NI;
+                System.out.print(NI + " ");
+                NI--;
+
+            } while (NI >= 1);
+            System.out.println();
+        }
+
+        System.out.println("O fatorial de " + NI +  " é: " + fatorial);
+
+        scanner.close();
     }
 }
