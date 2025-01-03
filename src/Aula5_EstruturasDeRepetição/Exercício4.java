3601fb19c924d70e47c2e2ec9baf67cbde9908b5
Mateus Vieira <mateusvs2420@gmail.com>
2025-01-03 00:54:36 -0300

    03/01/2024

--- src/Aula5_EstruturasDeRepetição/Exercício4.java
+++ src/Aula5_EstruturasDeRepetição/Exercício4.java
@@ -5,6 +5,28 @@ import java.util.Scanner;
 public class Exercício4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("");
+        int IP;
+        boolean isPrimo = true;
+        System.out.println("Digite um número inteiro positivo: ");
+        IP = scanner.nextInt();
+
+        if (IP <= 1) {
+            System.out.println("O número precisa ser maior que 1.");
+
+        } else {
+            for (int i = 2; i < IP; i++) {
+                if (IP % i == 0) ;
+                isPrimo = false;
+                break;
+            }
+            if (isPrimo = true) {
+                System.out.println("O número " + IP + " é Primo!! ");
+
+            } else {
+                System.out.println("O número " + IP + " não é Primo!! ");
+
+                scanner.close();
+            }
+        }
     }
 }
